00b6b84b5c0f4a5f889293fd0c1d81092d43be34
liebespaar93 <liebespaar93@gmail.com>
2024-04-02 12:02:30 +0900

    learn FlexWrap

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Button, Modal, StyleSheet, Text, View } from 'react-native';
+import { Button, Modal, ScrollView, StyleSheet, Text, View } from 'react-native';
 import ModalTutorial from './components/tutorial/ModalTutorial';
 import StatusBarTutorial from './components/tutorial/StatusBarTutorial';
 import ActivityIndicatorTutorial from './components/tutorial/ActivityIndicatorTutorial';
@@ -11,22 +11,24 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <BoxStyle style={{ backgroundColor: "#1c4c56", alignSelf: "flex-start" }}>1c4c56</BoxStyle>
-      <BoxStyle style={{ backgroundColor: "#ffbbbb", alignSelf: "stretch" }}>ffbbbb</BoxStyle>
-      <BoxStyle style={{ backgroundColor: "#1eae98", alignSelf: "auto" }}>1eae98</BoxStyle>
-      <BoxStyle style={{ backgroundColor: "#6fd6ff", alignSelf:"stretch",  alignItems:"flex-start" }}>6fd6ff</BoxStyle>
-      <BoxStyle style={{ backgroundColor: "#fe90af" }}>fe90af</BoxStyle>
-      <BoxStyle style={{ backgroundColor: "#ff8c8c" }}>ff8c8c</BoxStyle>
-      <BoxStyle style={{ backgroundColor: "#a890fe" }}>a890fe</BoxStyle>
-      <BoxStyle style={{ backgroundColor: "#ffffcf" }}>ffffcf</BoxStyle>
-      <BoxStyle style={{ backgroundColor: "#fca5f1" }}>fca5f1</BoxStyle>
+        <BoxStyle style={{ backgroundColor: "#1c4c56", alignSelf: "flex-start" }}>1c4c56</BoxStyle>
+        <BoxStyle style={{ backgroundColor: "#ffbbbb", alignSelf: "stretch" }}>ffbbbb</BoxStyle>
+        <BoxStyle style={{ backgroundColor: "#1eae98", alignSelf: "auto" }}>1eae98</BoxStyle>
+        <BoxStyle style={{ backgroundColor: "#6fd6ff", alignSelf: "stretch", alignItems: "flex-start" }}>6fd6ff</BoxStyle>
+        <BoxStyle style={{ backgroundColor: "#fe90af" }}>fe90af</BoxStyle>
+        <BoxStyle style={{ backgroundColor: "#ff8c8c" }}>ff8c8c</BoxStyle>
+        <BoxStyle style={{ backgroundColor: "#a890fe" }}>a890fe</BoxStyle>
+        <BoxStyle style={{ backgroundColor: "#ffffcf" }}>ffffcf</BoxStyle>
+        <BoxStyle style={{ backgroundColor: "#fca5f1" }}>fca5f1</BoxStyle>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
+    flexWrap: "wrap-reverse",
+    height: 300,
     alignItems: "flex-end",
     marginTop: 30,
     paddingTop: 30,
