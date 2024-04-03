b5b72c5a90af2ebae50117a4e598e21c0929cace
liebespaar93 <liebespaar93@gmail.com>
2024-04-03 23:21:44 +0900

    learn List

--- App.tsx
+++ App.tsx
@@ -1,56 +1,25 @@
-import { SafeAreaView, Platform, StyleSheet, View, useWindowDimensions, ScrollView } from 'react-native';
+import { SafeAreaView, Platform, StyleSheet, View, useWindowDimensions, ScrollView, Text } from 'react-native';
 import PokemonCard from './components/tutorial/PokemonCard/PokemonCard';
+
+import pokemonList from "@/data/pokemon.json"
 
 export default function App() {
   const dimensions = useWindowDimensions();
   const windowWidth = dimensions.width;
   const windowHeight = dimensions.height;
 
-  const charmanderData = {
-    name: "Charmander",
-    image: require("@/assets/pokemon/charmander.png"),
-    type: "Fire",
-    hp: 39,
-    moves: ["Scratch", "Ember", "Growl", "Leer"],
-    weaknesses: ["Water", "Rock"],
-  };
-
-  const squirtleData = {
-    name: "Squirtle",
-    image: require("@/assets/pokemon/squirtle.png"), // Replace with the actual image path
-    type: "Water",
-    hp: 44,
-    moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
-    weaknesses: ["Electric", "Grass"],
-  };
-
-  const bulbasaurData = {
-    name: "Bulbasaur",
-    image: require("@/assets/pokemon/bulbasaur.png"), // Replace with the actual image path
-    type: "Grass",
-    hp: 45,
-    moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
-    weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
-  };
-
-  const pikachuData = {
-    name: "Pikachu",
-    image: require("@/assets/pokemon/pikachu.png"), // Replace with the actual image path
-    type: "Electric",
-    hp: 35,
-    moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
-    weaknesses: ["Ground"],
-  };
-
-  console.log(charmanderData)
   return (
     <SafeAreaView style={styles.safeContainer}>
       <View style={styles.container}>
-        <ScrollView>
-          <PokemonCard {...charmanderData} />
-          <PokemonCard {...squirtleData} />
-          <PokemonCard {...bulbasaurData} />
-          <PokemonCard {...pikachuData} />
+        <ScrollView style={styles.scrollView}>
+          {pokemonList.map((pokemon, index) => {
+            return (
+              <View style={styles.card} key={pokemon.id}>
+                <Text style={styles.cardText}>{pokemon.type}</Text>
+                <Text style={styles.cardText}>{pokemon.name}</Text>
+              </View>
+            )
+          })}
         </ScrollView>
       </View>
     </SafeAreaView>
@@ -64,7 +33,18 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    alignItems: "center",
-    backgroundColor: Platform.OS === "ios" ? '#ffa13f' : '#ffdfcf'
+  },
+  scrollView: {
+    paddingHorizontal: 16
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    marginBottom: 16
+  },
+  cardText: {
+    fontSize: 24
   },
 });
--- docs/react_native.md
+++ docs/react_native.md
@@ -1184,3 +1184,60 @@ const styles = StyleSheet.create({
 })
 export default PokemonCard
 ```
+
+## List
+
+react 와 같은 방식으로 map을 이용해 요소들을 하나하나 함수들 돌려 보여주는거 같다
+
+```jsx
+import { SafeAreaView, Platform, StyleSheet, View, useWindowDimensions, ScrollView, Text } from 'react-native';
+import PokemonCard from './components/tutorial/PokemonCard/PokemonCard';
+
+import pokemonList from "@/data/pokemon.json"
+
+export default function App() {
+  const dimensions = useWindowDimensions();
+  const windowWidth = dimensions.width;
+  const windowHeight = dimensions.height;
+
+  return (
+    <SafeAreaView style={styles.safeContainer}>
+      <View style={styles.container}>
+        <ScrollView style={styles.scrollView}>
+          {pokemonList.map((pokemon, index) => {
+            return (
+              <View style={styles.card} key={pokemon.id}>
+                <Text style={styles.cardText}>{pokemon.type}</Text>
+                <Text style={styles.cardText}>{pokemon.name}</Text>
+              </View>
+            )
+          })}
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    backgroundColor: '#ffdfcf'
+  },
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    paddingHorizontal: 16
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    marginBottom: 16
+  },
+  cardText: {
+    fontSize: 24
+  },
+});
+```
