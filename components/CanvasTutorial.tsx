f6cb9f8914c5f5bb778532ce072c08fdbd9ccab7
liebespaar93 <liebespaar93@gmail.com>
2024-04-11 16:35:22 +0900

    just web running

--- components/CanvasTutorial.tsx
+++ components/CanvasTutorial.tsx
@@ -1,5 +1,5 @@
 import { View, Text } from 'react-native'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { Suspense, useEffect, useRef, useState } from 'react'
 import { Canvas, useFrame, useLoader } from '@react-three/fiber/native'
 import { Group, Mesh, Object3DEventMap } from 'three'
 
@@ -28,41 +28,26 @@ import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { useAssets } from 'expo-asset';
-function ShoeTutorial() {
-    const loadfile = require('@/assets/models/shoe.obj')
-    console.log(loadfile)
-    const helloAsset = useAssets(loadfile);
-    console.log(helloAsset[0])
+import { OrbitControls, useGLTF } from '@react-three/drei/native';
+import { color } from 'three/examples/jsm/nodes/shadernode/ShaderNode';
+import { ShoeModel } from './ShoeModel';
 
-    const obj = new OBJLoader();
-    const gltf = new OBJLoader();
-    if (helloAsset[0])
-        obj.load(helloAsset[0][0].localUri!,
-            (data) => {
-                console.log(data)
-            },
-            (xhr) => {
-                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-            },
-            (error) => {
-                console.log(error, 'An error happened')
-            }
-        )
+
+function ShoeTutorial() {
     return (
-        <mesh>
-            <boxGeometry></boxGeometry>
-            {/* <primitive object={{ obj }} scole={10} /> */}
-        </mesh>
+        <ShoeModel></ShoeModel>
     )
 }
 export default function CanvasTutorial() {
     return (
-        <View style={{ flex: 1 }}>
+        <View style={{ width: 400, height: 400 }}>
             <Text style={{ flex: 0.5, color: "#fff" }}>CanvasTutorial</Text>
-            <Canvas style={{ flex: 1 }}>
+            <Canvas style={{ flex: 1, backgroundColor: '#000' }}>
                 <ambientLight intensity={1} color={'#fff'} />
-                {/* <BoxTutorial /> */}
-                <ShoeTutorial />
+                <spotLight position={[0, 2, 1]} intensity={10}></spotLight>
+                <Suspense fallback={null}>
+                    <ShoeTutorial />
+                </Suspense>
             </Canvas>
         </View>
     )
